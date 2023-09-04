67e282dbdc3cdf381542d703fd79bd7bc92dc3ff
m-h09 <ermynb0srstgojpn26@gmail.com>
2023-09-04 16:43:53 +0900

    イベント発火の基礎
    mouseoverとmouseoutの動作確認

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('mouseover',function(){
+    const pullDownButton = document.getElementsByClassName("lists");
+        console.log("mouseover OK")
+})
+
+window.addEventListener('mouseout',function(){
+    const pullDownButton = document.getElementsByClassName("lists");
+        console.log("mouseout OK")
+})
