b5363f2ef45042d7a8218ee80829eab073822785
m-h09 <ermynb0srstgojpn26@gmail.com>
2023-09-04 19:01:58 +0900

    イベント発火基礎編
    Javascript基礎10

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-window.addEventListener('mouseover',function(){
-    const pullDownButton = document.getElementsByClassName("lists");
-        console.log("mouseover OK")
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener('mouseover',function(){
+        pullDownButton.setAttribute("style","background-color:blue;")
+        console.log("乗ったときはブルー")
+    })
 })
 
-window.addEventListener('mouseout',function(){
-    const pullDownButton = document.getElementsByClassName("lists");
-        console.log("mouseout OK")
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener('mouseout',function(){
+        pullDownButton.removeAttribute("style","background-color:red;")
+    })
+        console.log("離れた時はレッドに戻る")
 })
+
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener('click',function(){
+        pullDownButton.setAttribute("style","background-color:green;")
+        console.log("クリックすると緑")
+    })
+})
+
+
