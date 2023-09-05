19b68fd302988ed717fe90cc43c8339bbf10b0b7
m-h09 <ermynb0srstgojpn26@gmail.com>
2023-09-05 23:36:01 +0900

    条件分岐編
    js11基礎

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener('mouseover',function(){
         pullDownButton.setAttribute("style","background-color:blue;")
+    this.addEventListener('mouseover',function(){
+        this.setAttribute("style","background-color:blue;")
         console.log("乗ったときはブルー")
     })
 })
@@ -10,6 +12,8 @@ window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener('mouseout',function(){
         pullDownButton.removeAttribute("style","background-color:red;")
+    this.addEventListener('mouseout',function(){
+        this.removeAttribute("style","background-color:red;")
     })
         console.log("離れた時はレッドに戻る")
 })
@@ -19,6 +23,14 @@ window.addEventListener('load',function(){
     pullDownButton.addEventListener('click',function(){
         pullDownButton.setAttribute("style","background-color:green;")
         console.log("クリックすると緑")
+       if(pullDownParents.getAttribute("style")== "display:block;"){
+        this.removeAttribute("style","display:block;")
+        console.log("非表示")
+       }else {
+        thiss.setAttribute("style","display:block;")
+        console.log("表示")
+       }
+    
     })
 })
 
