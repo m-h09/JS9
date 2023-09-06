670990e8d5c7c9d2833306f7cb0cfe18919821ef
m-h09 <ermynb0srstgojpn26@gmail.com>
2023-09-07 05:49:22 +0900

    js11基礎修正部分
    削除した項目について
    thisに変えても動作していたため続行していたが、それは更新ボタンを押して確認ができなかったためであった
    
    実際は今の通りで動作確認できている

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener('mouseover',function(){
         pullDownButton.setAttribute("style","background-color:blue;")
-    this.addEventListener('mouseover',function(){
         this.setAttribute("style","background-color:blue;")
         console.log("乗ったときはブルー")
     })
@@ -12,7 +11,6 @@ window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener('mouseout',function(){
         pullDownButton.removeAttribute("style","background-color:red;")
-    this.addEventListener('mouseout',function(){
         this.removeAttribute("style","background-color:red;")
     })
         console.log("離れた時はレッドに戻る")
@@ -24,10 +22,8 @@ window.addEventListener('load',function(){
         pullDownButton.setAttribute("style","background-color:green;")
         console.log("クリックすると緑")
        if(pullDownParents.getAttribute("style")== "display:block;"){
-        this.removeAttribute("style","display:block;")
         console.log("非表示")
        }else {
-        thiss.setAttribute("style","display:block;")
         console.log("表示")
        }
     
